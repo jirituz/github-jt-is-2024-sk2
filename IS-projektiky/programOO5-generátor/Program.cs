1274d16f9e5b4ad3bee7a739b3bc752cde5d207b
JiriTuz <jiri.tuz@tul.cz>
2024-10-24 13:06:35 +0200

    Vtvoření základní struktury generátoru

---
+++ IS-projektiky/programOO5-generátor/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program {
+
+    static void Main() {
+
+        // chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("******* Generátor pseudonáhodných čísel *******");
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("******** Jiří Tuž *****************************");
+            Console.WriteLine("*******************************************\n\n");
+            Console.WriteLine();
+
+            // Vstup od uživatele - lepší varianta
+            Console.Write("Zadejte počet generovaných čísel ");
+            int n;
+            while(!int.TryParse(Console.ReadLine(), out n)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu první číslo řady (celé číslo): ");
+            }
+
+             Console.Write("Zadejte dolní mez (celé číslo) ");
+            int dm;
+            while(!int.TryParse(Console.ReadLine(), out dm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu první číslo řady (celé číslo): ");
+            }
+
+             Console.Write("Zadejte Horní mez (celé číslo) ");
+            int hm;
+            while(!int.TryParse(Console.ReadLine(), out hm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu první číslo řady (celé číslo): ");
+            }
+            
+            Console.WriteLine("\n\n===================");
+            Console.WriteLine("\n\nuživatelský vstup");
+            Console.WriteLine("\n\npočet čísel: {0}; dolní mez: {1}; Horní mez: {2}", n, dm, hm);
+            Console.WriteLine("\n\n===================");
+            Console.WriteLine("\n\n===================\n\n");
+
+
+
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
+    }   
+
+
+}
