fe8e1fc9d3171ab22faab6dfc28ff42eaed4139a
JiriTuz <jiri.tuz@tul.cz>
2024-11-05 20:18:10 +0100

    #3 Vykreselení písmena N DONE

--- IS-projektiky/Program004-vykreslovani_obrazcu/Program.cs
+++ IS-projektiky/Program004-vykreslovani_obrazcu/Program.cs
@@ -15,13 +15,13 @@ class Program
         Console.WriteLine("*****Vykreslování*****");
         Console.WriteLine("**********************");
 
-
-            // Vstup od uživatele - lepší varianta
-            Console.Write("Zadejte velikost obrazce (celé číslo): ");
-            int n;
-            while(!int.TryParse(Console.ReadLine(), out n)) {
-                Console.Write("Nezadali jste celé číslo. Zadejte znovu velikost obrazce (celé číslo): ");
-            }
+//kosočtverec
+             // Vstup od uživatele - lepší varianta
+            // Console.Write("Zadejte velikost obrazce (celé číslo): ");
+            // int n;
+            // while(!int.TryParse(Console.ReadLine(), out n)) {
+            //     Console.Write("Nezadali jste celé číslo. Zadejte znovu velikost obrazce (celé číslo): ");
+            // }
 
 
         // // horní část kosočtverce
@@ -63,44 +63,74 @@ class Program
             
         //     Console.WriteLine();
 
-        //PŘESÍPACÍ HODINY 
+//PŘESÍPACÍ HODINY 
 
         //horní část hodin přesípacích hodin
-        for (int i = n ; i >= 1; i--)
+        // for (int i = n ; i >= 1; i--)
+        // {
+        //     // vypsání mezer
+        //     for (int j = n; j > i; j--)
+        //     {
+        //         Console.Write(" ");
+        //     }
+
+        //     // vypsání hvězd
+        //     for (int j = 1; j <= (2 * i ); j++)
+        //     {
+        //         Console.Write("*");
+        //     }
+        //     Console.WriteLine();
+        //     }
+
+
+
+        // // spodní část přesípacích hodin
+        // for (int i = 1; i <= n; i++)
+        // {
+        //     // vypsání mezer
+        //     for (int j = i; j < n; j++)
+        //     {
+        //         Console.Write(" ");
+        //     }
+
+        //     // vypsání hvězd
+        //     for (int j = 1; j <= (2 * i ); j++)
+        //     {
+        //         Console.Write("*");
+        //     }
+
+        //     Console.WriteLine();
+        // }
+
+ //vykreslení pímena N
+            //vstup uživatele 
+            Console.Write("Zadejte velikost obrazce minimální velikost 2 (celé číslo): ");
+            int n;
+            while(!int.TryParse(Console.ReadLine(), out n) || n < 2) {
+                Console.Write("nezadali jste celé číslo, Zadejte znovu velikost obrazce alespoň 2 (celé číslo)");
+            }
+           for (int i = 0; i <= n; i++)
         {
-            // vypsání mezer
-            for (int j = n; j > i; j--)
+            // Llevá strana
+            Console.Write("*");
+
+            // uhlopříčka
+            for (int j = 0; j <= n; j++)
             {
-                Console.Write(" ");
+                if (j == i)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
             }
 
-            // vypsání hvězd
-            for (int j = 1; j <= (2 * i ); j++)
-            {
-                Console.Write("*");
-            }
-            Console.WriteLine();
-            }
-
-
-
-        // spodní část přesípacích hodin
-        for (int i = 1; i <= n; i++)
-        {
-            // vypsání mezer
-            for (int j = i; j < n; j++)
-            {
-                Console.Write(" ");
-            }
-
-            // vypsání hvězd
-            for (int j = 1; j <= (2 * i ); j++)
-            {
-                Console.Write("*");
-            }
-
-            Console.WriteLine();
+            // pravá strana
+            Console.WriteLine("*");
         }
+
             
             // Opakování programu
             Console.WriteLine("Pro opakování programu stiskněte klávesu a");
